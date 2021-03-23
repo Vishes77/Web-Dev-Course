d5d643a092c1c79943f91a06957cbe9989a5ac25
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2021-03-23 19:13:00 +0530

    JavaScript Basics and level up added.

---
+++ 19 - JavaScript Loops/IteratingObjects/app.js
@@ -0,0 +1,34 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+// ===================================
+// USING FOR...IN TO ITERATE AN OBJECT
+// ===================================
+// for..of loop is not for object literals.
+
+// for (let person in testScores) {
+//     console.log(`${person} scored ${testScores[person]}`);
+// }
+
+// ==============================================
+// Using Object methods to iterate
+// (turn data into an array and then use for...of)
+// ==============================================
+
+let total = 0;
+let scores = Object.values(testScores);
+// above lines return a array of scores.
+for (let score of scores) {
+    total += score;
+}
+console.log(total / scores.length)
+
